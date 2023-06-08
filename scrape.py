d3c0086f392e0f68eaad7e7c546810f6798d441c
wusadhane <83650015+wusadhane@users.noreply.github.com>
2023-06-08 23:46:03 +0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,9 +1,23 @@
 from bs4 import BeautifulSoup
 import requests 
+import pandas as pd
+import streamlit as st
 
-soup = BeautifulSoup(requests.get("https://www.tradingview.com/markets/stocks-indonesia/sectorandindustry-industry/marine-shipping/").text)
+st.set_page_config(page_title="optimasi", layout="wide")
+
+df = pd.DataFrame()
+
+# variable global dataframe
+stock_name_list = []
+stock_price_list = []
+stock_symbol_list = []
+
+# link url target
+url = BeautifulSoup(requests.get("https://www.tradingview.com/markets/stocks-indonesia/sectorandindustry-industry/marine-shipping/").text)
+
 # first we should find our table object:
-table = soup.find('table')
+table = url.find('table')
+
 # then we can iterate through each row and extract either header or row values:
 header = []
 rows = []
@@ -13,7 +27,19 @@ for i, row in enumerate(table.find_all('tr')):
     else:
         rows.append([el.text.strip() for el in row.find_all('td')])
 
-print(header)
 
+print(header)
 for row in rows:
     print(row)
+
+def scrape(date):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    tr = soup.find_all('tr',attrs={'class': 'row-RdUXZpkv'})
+    count = 10
+    for row in tr:
+        if count == 10:
+            break
+        count = count + 1
+        stock_name = row.find('th', attrs={
+            'class': 'cell-seAzPAHn cell-fixed-RHkwFEqU onscroll-shadow'})
